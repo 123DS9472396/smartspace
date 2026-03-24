8bfcc69f8e06edea857b6db077d9d3fb195a24eb
Dipesh Sharma <140637989+123DS9472396@users.noreply.github.com>
2026-03-25 02:48:41 +0530

    Added chunk-EMLCZFSA.js

---
+++ warehouse_2026_vite_cache/deps/@radix-ui_react-collapsible.js
@@ -0,0 +1,31 @@
+"use client";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  Content,
+  Root,
+  Trigger,
+  createCollapsibleScope
+} from "./chunk-FUKWGDUE.js";
+import "./chunk-N3XXQJTN.js";
+import "./chunk-M6RB35O7.js";
+import "./chunk-WZRH7XJJ.js";
+import "./chunk-P4S5NJOQ.js";
+import "./chunk-HU2Q2UQQ.js";
+import "./chunk-WPKJTQFJ.js";
+import "./chunk-JQAHHWRU.js";
+import "./chunk-OXQCQE4P.js";
+import "./chunk-NZAIND7N.js";
+import "./chunk-RHY4FV2B.js";
+import "./chunk-UVNPGZG7.js";
+import "./chunk-OL46QLBJ.js";
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  Content,
+  Root,
+  Trigger,
+  createCollapsibleScope
+};
